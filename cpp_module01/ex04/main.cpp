0a352c563a630d710e6aee5a17779b7a7d7b5331
vantunes <valerio.as@gmail.com>
2021-10-27 14:47:09 +0100

    change caracter by carater to functions in cpp

--- cpp_module01/ex04/main.cpp
+++ cpp_module01/ex04/main.cpp
@@ -1,35 +1,22 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 
 int error(int i)
 {
 	std::string Error[2] = {"File Error", "Arg Error"};
-
     std::cout << Error[i] << '\n';
     return (i);
 }
 
 std::string replace_str(std::string line, std::string s1, std::string s2)
 {
-	std::stringstream ss;
-	size_t j;
-	size_t len(s1.length());
-    size_t i = -1;
-	while (++i < line.length())
-	{
-		j = 0;
-		while (line[i + j] == s1[j] && j < len)
-			j++;
-		if (j == len)
-		{
-			ss << s2;
-			i += (j - 1);
-		}
-		else
-			ss << line[i];
-	}
-	return (ss.str());
+	int pos;
+	pos = line.find(s1);
+	if (pos < 0)
+		return (line);
+	line.erase(pos, s1.length());
+	line.insert(pos, s2);
+	return (line);
 }
 
 int main(int argc, char **argv)
